0aaddba67fe5d58b9a088cb25d51f8cd8c64be1d
Mara <mara-alexandra.nedelcu@my.fmi.unibuc.ro>
2021-07-15 19:48:26 +0300

    test

--- src/main/java/com/newsapp/NewsappApplication.java
+++ src/main/java/com/newsapp/NewsappApplication.java
@@ -9,5 +9,6 @@ public class NewsappApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewsappApplication.class, args);
         System.out.println("A PORNIT\n");
+        
     }
 }
